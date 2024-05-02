3dca1b69850c652c234a4055af1386f58bd4a321
simonehayden <168015429+simonehayden@users.noreply.github.com>
2024-05-02 11:19:25 -0700

    Update Unit5HW2.py

--- Unit5HW2.py
+++ Unit5HW2.py
@@ -36,11 +36,11 @@ def problem_five():
 def fizz_buzz():
     number:(int(input("enter a number: ")
     for i in range(1,number + 1):
-        if i % 3 == 0 and i % 5:
+        if i % 3 == 0:
             print ("FizzBuzz")
-        elif i % 3 == 0:
+        elif i % 5== 0:
             print("Fizz")
-        elif i % 5:
+        elif i % 15==0:
             print("Buzz")
         else:
             print(i)
